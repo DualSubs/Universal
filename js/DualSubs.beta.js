22e161041ca9813cfa4b95e1d4ca51e6cd9af74c
Virgil Clyne <Virgil@nanocat.me>
2022-03-07 14:34:41 +0800

    Update WebVTT.min.js to v1.3.0
    Update DualSubs.beta.js

--- js/DualSubs.beta.js
+++ js/DualSubs.beta.js
@@ -388,15 +388,17 @@ $.log(`🚧 ${$.name}, Enviroment调试信息`, `$.Cache类型: ${typeof $.Cache
 			/***************** merge Dual Subtitles *****************/
 			let FirstSub = VTT.parse(body, ["timeStamp", "ms"])
 			$.log("FirstSub.headers", JSON.stringify(FirstSub.headers))
+			$.log("FirstSub.CSS", JSON.stringify(FirstSub.CSS))
 			$.log("FirstSub.body[0]", JSON.stringify(FirstSub.body[0]))
 			$.log("FirstSub.body[10]", JSON.stringify(FirstSub.body[10]))
-			$.log("FirstSub.body[25].groups", JSON.stringify(FirstSub.body[25].groups))
 			let SecondSub = VTT.parse(result, ["timeStamp", "ms"])
 			$.log("SecondSub.headers", JSON.stringify(SecondSub.headers))
+			$.log("SecondSub.CSS", JSON.stringify(SecondSub.CSS))
 			$.log("SecondSub.body[0]", JSON.stringify(SecondSub.body[0]))
 			$.log("SecondSub.body[10]", JSON.stringify(SecondSub.body[10]))
-			$.log("SecondSub.body[25].groups", JSON.stringify(SecondSub.body[25].groups))
+			
 			//$.log("VTT.stringify(FirstSub)", VTT.stringify(FirstSub));
+
 			let DualSub = await mergeDualSubs(FirstSub, SecondSub);
 			//$.log(`🚧 ${$.name}, merge Dual Subtitles`, "await mergeDualSubs(FirstSub, SecondSub)", `DualSub内容: ${JSON.stringify(DualSub)}`, "");
 			DualSub = VTT.stringify(DualSub)
@@ -505,12 +507,20 @@ async function getTranslateSubtitles(body) {
 
 // Function 6
 // Combine Dual Subtitles
-async function mergeDualSubs(Sub1 = { headers: {}, body: [] }, Sub2 = { headers: {}, body: [] }, options = ["Forward"]) { // options = ["Forward", "Reverse"]
+async function mergeDualSubs(Sub1 = { headers: {}, CSS: {}, body: [] }, Sub2 = { headers: {}, CSS: {}, body: [] }, options = ["Forward"]) { // options = ["Forward", "Reverse"]
 	$.log(`🚧 ${$.name}, Combine Dual Subtitles`, "mergeDualSubs", "");
+	//$.log(`🚧 ${$.name}, Combine Dual Subtitles`, "mergeDualSubs", `Sub1内容: ${JSON.stringify(Sub1)}`, "");
+	//$.log(`🚧 ${$.name}, Combine Dual Subtitles`, "mergeDualSubs", `Sub2内容: ${JSON.stringify(Sub2)}`, "");
+
+	let DualSub = options.includes("Reverse") ? Sub2 : Sub1
+	/*
 	let DualSub = {
 		headers: options.includes("Reverse") ? Sub2.headers : Sub1.headers,
+		CSS: options.includes("Reverse") ? Sub2.CSS : Sub1.CSS,
 		body: options.includes("Reverse") ? Sub2.body : Sub1.body
 	};
+	*/
+	//$.log(`🚧 ${$.name}, Combine Dual Subtitles`, "mergeDualSubs", `let DualSub内容: ${JSON.stringify(DualSub)}`, "");
 	// 有序数列 用不着排序
 	//FirstSub.body.sort((x, y) => x - y);
 	//SecondSub.body.sort((x, y) => x - y);
@@ -533,6 +543,7 @@ async function mergeDualSubs(Sub1 = { headers: {}, body: [] }, Sub2 = { headers:
 			index2++;
 		}
 	}
+	//$.log(`🚧 ${$.name}, Combine Dual Subtitles`, "mergeDualSubs", `return DualSub内容: ${JSON.stringify(DualSub)}`, "");
 	return DualSub;
 };
 
@@ -659,57 +670,5 @@ async function groupAgain(data, num) {
 function Env(t,e){class s{constructor(t){this.env=t}send(t,e="GET"){t="string"==typeof t?{url:t}:t;let s=this.get;return"POST"===e&&(s=this.post),new Promise((e,i)=>{s.call(this,t,(t,s,r)=>{t?i(t):e(s)})})}get(t){return this.send.call(this.env,t)}post(t){return this.send.call(this.env,t,"POST")}}return new class{constructor(t,e){this.name=t,this.http=new s(this),this.data=null,this.dataFile="box.dat",this.logs=[],this.isMute=!1,this.isNeedRewrite=!1,this.logSeparator="\n",this.encoding="utf-8",this.startTime=(new Date).getTime(),Object.assign(this,e),this.log("",`🔔${this.name}, 开始!`)}isNode(){return"undefined"!=typeof module&&!!module.exports}isQuanX(){return"undefined"!=typeof $task}isSurge(){return"undefined"!=typeof $httpClient&&"undefined"==typeof $loon}isLoon(){return"undefined"!=typeof $loon}isShadowrocket(){return"undefined"!=typeof $rocket}toObj(t,e=null){try{return JSON.parse(t)}catch{return e}}toStr(t,e=null){try{return JSON.stringify(t)}catch{return e}}getjson(t,e){let s=e;const i=this.getdata(t);if(i)try{s=JSON.parse(this.getdata(t))}catch{}return s}setjson(t,e){try{return this.setdata(JSON.stringify(t),e)}catch{return!1}}getScript(t){return new Promise(e=>{this.get({url:t},(t,s,i)=>e(i))})}runScript(t,e){return new Promise(s=>{let i=this.getdata("@chavy_boxjs_userCfgs.httpapi");i=i?i.replace(/\n/g,"").trim():i;let r=this.getdata("@chavy_boxjs_userCfgs.httpapi_timeout");r=r?1*r:20,r=e&&e.timeout?e.timeout:r;const[o,h]=i.split("@"),a={url:`http://${h}/v1/scripting/evaluate`,body:{script_text:t,mock_type:"cron",timeout:r},headers:{"X-Key":o,Accept:"*/*"}};this.post(a,(t,e,i)=>s(i))}).catch(t=>this.logErr(t))}loaddata(){if(!this.isNode())return{};{this.fs=this.fs?this.fs:require("fs"),this.path=this.path?this.path:require("path");const t=this.path.resolve(this.dataFile),e=this.path.resolve(process.cwd(),this.dataFile),s=this.fs.existsSync(t),i=!s&&this.fs.existsSync(e);if(!s&&!i)return{};{const i=s?t:e;try{return JSON.parse(this.fs.readFileSync(i))}catch(t){return{}}}}}writedata(){if(this.isNode()){this.fs=this.fs?this.fs:require("fs"),this.path=this.path?this.path:require("path");const t=this.path.resolve(this.dataFile),e=this.path.resolve(process.cwd(),this.dataFile),s=this.fs.existsSync(t),i=!s&&this.fs.existsSync(e),r=JSON.stringify(this.data);s?this.fs.writeFileSync(t,r):i?this.fs.writeFileSync(e,r):this.fs.writeFileSync(t,r)}}lodash_get(t,e,s){const i=e.replace(/\[(\d+)\]/g,".$1").split(".");let r=t;for(const t of i)if(r=Object(r)[t],void 0===r)return s;return r}lodash_set(t,e,s){return Object(t)!==t?t:(Array.isArray(e)||(e=e.toString().match(/[^.[\]]+/g)||[]),e.slice(0,-1).reduce((t,s,i)=>Object(t[s])===t[s]?t[s]:t[s]=Math.abs(e[i+1])>>0==+e[i+1]?[]:{},t)[e[e.length-1]]=s,t)}getdata(t){let e=this.getval(t);if(/^@/.test(t)){const[,s,i]=/^@(.*?)\.(.*?)$/.exec(t),r=s?this.getval(s):"";if(r)try{const t=JSON.parse(r);e=t?this.lodash_get(t,i,""):e}catch(t){e=""}}return e}setdata(t,e){let s=!1;if(/^@/.test(e)){const[,i,r]=/^@(.*?)\.(.*?)$/.exec(e),o=this.getval(i),h=i?"null"===o?null:o||"{}":"{}";try{const e=JSON.parse(h);this.lodash_set(e,r,t),s=this.setval(JSON.stringify(e),i)}catch(e){const o={};this.lodash_set(o,r,t),s=this.setval(JSON.stringify(o),i)}}else s=this.setval(t,e);return s}getval(t){return this.isSurge()||this.isLoon()?$persistentStore.read(t):this.isQuanX()?$prefs.valueForKey(t):this.isNode()?(this.data=this.loaddata(),this.data[t]):this.data&&this.data[t]||null}setval(t,e){return this.isSurge()||this.isLoon()?$persistentStore.write(t,e):this.isQuanX()?$prefs.setValueForKey(t,e):this.isNode()?(this.data=this.loaddata(),this.data[e]=t,this.writedata(),!0):this.data&&this.data[e]||null}initGotEnv(t){this.got=this.got?this.got:require("got"),this.cktough=this.cktough?this.cktough:require("tough-cookie"),this.ckjar=this.ckjar?this.ckjar:new this.cktough.CookieJar,t&&(t.headers=t.headers?t.headers:{},void 0===t.headers.Cookie&&void 0===t.cookieJar&&(t.cookieJar=this.ckjar))}get(t,e=(()=>{})){if(t.headers&&(delete t.headers["Content-Type"],delete t.headers["Content-Length"]),this.isSurge()||this.isLoon())this.isSurge()&&this.isNeedRewrite&&(t.headers=t.headers||{},Object.assign(t.headers,{"X-Surge-Skip-Scripting":!1})),$httpClient.get(t,(t,s,i)=>{!t&&s&&(s.body=i,s.statusCode=s.status),e(t,s,i)});else if(this.isQuanX())this.isNeedRewrite&&(t.opts=t.opts||{},Object.assign(t.opts,{hints:!1})),$task.fetch(t).then(t=>{const{statusCode:s,statusCode:i,headers:r,body:o}=t;e(null,{status:s,statusCode:i,headers:r,body:o},o)},t=>e(t));else if(this.isNode()){let s=require("iconv-lite");this.initGotEnv(t),this.got(t).on("redirect",(t,e)=>{try{if(t.headers["set-cookie"]){const s=t.headers["set-cookie"].map(this.cktough.Cookie.parse).toString();s&&this.ckjar.setCookieSync(s,null),e.cookieJar=this.ckjar}}catch(t){this.logErr(t)}}).then(t=>{const{statusCode:i,statusCode:r,headers:o,rawBody:h}=t;e(null,{status:i,statusCode:r,headers:o,rawBody:h},s.decode(h,this.encoding))},t=>{const{message:i,response:r}=t;e(i,r,r&&s.decode(r.rawBody,this.encoding))})}}post(t,e=(()=>{})){const s=t.method?t.method.toLocaleLowerCase():"post";if(t.body&&t.headers&&!t.headers["Content-Type"]&&(t.headers["Content-Type"]="application/x-www-form-urlencoded"),t.headers&&delete t.headers["Content-Length"],this.isSurge()||this.isLoon())this.isSurge()&&this.isNeedRewrite&&(t.headers=t.headers||{},Object.assign(t.headers,{"X-Surge-Skip-Scripting":!1})),$httpClient[s](t,(t,s,i)=>{!t&&s&&(s.body=i,s.statusCode=s.status),e(t,s,i)});else if(this.isQuanX())t.method=s,this.isNeedRewrite&&(t.opts=t.opts||{},Object.assign(t.opts,{hints:!1})),$task.fetch(t).then(t=>{const{statusCode:s,statusCode:i,headers:r,body:o}=t;e(null,{status:s,statusCode:i,headers:r,body:o},o)},t=>e(t));else if(this.isNode()){let i=require("iconv-lite");this.initGotEnv(t);const{url:r,...o}=t;this.got[s](r,o).then(t=>{const{statusCode:s,statusCode:r,headers:o,rawBody:h}=t;e(null,{status:s,statusCode:r,headers:o,rawBody:h},i.decode(h,this.encoding))},t=>{const{message:s,response:r}=t;e(s,r,r&&i.decode(r.rawBody,this.encoding))})}}time(t,e=null){const s=e?new Date(e):new Date;let i={"M+":s.getMonth()+1,"d+":s.getDate(),"H+":s.getHours(),"m+":s.getMinutes(),"s+":s.getSeconds(),"q+":Math.floor((s.getMonth()+3)/3),S:s.getMilliseconds()};/(y+)/.test(t)&&(t=t.replace(RegExp.$1,(s.getFullYear()+"").substr(4-RegExp.$1.length)));for(let e in i)new RegExp("("+e+")").test(t)&&(t=t.replace(RegExp.$1,1==RegExp.$1.length?i[e]:("00"+i[e]).substr((""+i[e]).length)));return t}msg(e=t,s="",i="",r){const o=t=>{if(!t)return t;if("string"==typeof t)return this.isLoon()?t:this.isQuanX()?{"open-url":t}:this.isSurge()?{url:t}:void 0;if("object"==typeof t){if(this.isLoon()){let e=t.openUrl||t.url||t["open-url"],s=t.mediaUrl||t["media-url"];return{openUrl:e,mediaUrl:s}}if(this.isQuanX()){let e=t["open-url"]||t.url||t.openUrl,s=t["media-url"]||t.mediaUrl,i=rawOpts["update-pasteboard"]||rawOpts.updatePasteboard;return{"open-url":e,"media-url":s,"update-pasteboard":i}}if(this.isSurge()){let e=t.url||t.openUrl||t["open-url"];return{url:e}}}};if(this.isMute||(this.isSurge()||this.isLoon()?$notification.post(e,s,i,o(r)):this.isQuanX()&&$notify(e,s,i,o(r))),!this.isMuteLog){let t=["","==============📣系统通知📣=============="];t.push(e),s&&t.push(s),i&&t.push(i),console.log(t.join("\n")),this.logs=this.logs.concat(t)}}log(...t){t.length>0&&(this.logs=[...this.logs,...t]),console.log(t.join(this.logSeparator))}logErr(t,e){const s=!this.isSurge()&&!this.isQuanX()&&!this.isLoon();s?this.log("",`❗️${this.name}, 错误!`,t.stack):this.log("",`❗️${this.name}, 错误!`,t)}wait(t){return new Promise(e=>setTimeout(e,t))}done(t={}){const e=(new Date).getTime(),s=(e-this.startTime)/1e3;this.log("",`🔔${this.name}, 结束! 🕛 ${s} 秒`),this.log(),(this.isSurge()||this.isQuanX()||this.isLoon())&&$done(t)}}(t,e)}
 
 // https://github.com/DualSubs/WebVTT/blob/main/WebVTT.min.js
-function WebVTT(e,t){return new class{constructor(e,t){this.name=e,Object.assign(this,t)}parse(e=new String,t=["timeStamp"]){const d=t.includes("ms")?/^(?:(?<srtNum>\d+)[(\r\n)\r\n])?(?<timeLine>(?<startTime>(?:\d\d:)?\d\d:\d\d(?:\.|,)\d\d\d) --> (?<endTime>(?:\d\d:)?\d\d:\d\d(?:\.|,)\d\d\d)) ?(?<options>.+)?[^](?<text>.+)/:/^(?:(?<srtNum>\d+)[(\r\n)\r\n])?(?<timeLine>(?<startTime>(?:\d\d:)?\d\d:\d\d)(?:\.|,)\d\d\d --> (?<endTime>(?:\d\d:)?\d\d:\d\d)(?:\.|,)\d\d\d) ?(?<options>.+)?[^](?<text>.+)/;let n={headers:e.match(/^(?:(?<fileType>WEBVTT)[^][^])?(?:(?<CSSStyle>STYLE)[^](?<CSSboxes>.*::cue.*(?:\(.*\))?(?:(?:\n|.)*})?)[^][^])?/)?.groups??null,body:e.split(/[(\r\n)\r\n]{2,}/).map((e=>e.match(d)?.groups??""))};return n.body=n.body.filter(Boolean),n.body=n.body.map(((e,d)=>{if(e.index=d,"WEBVTT"!==n.headers?.fileType&&(e.timeLine=e.timeLine.replace(",","."),e.startTime=e.startTime.replace(",","."),e.endTime=e.endTime.replace(",",".")),t.includes("timeStamp")){let d=e.startTime.replace(/(.*)/,"1970-01-01T$1Z");e.timeStamp=t.includes("ms")?Date.parse(d):Date.parse(d)/1e3}return t.includes("multiText")&&(e.text=e.text.split(/[(\r\n)\r\n]/)),e})),n}stringify(e={headers:new Object,body:new Array},t=["milliseconds","\n"]){const d=t.includes("\n")?"\n":t.includes("\r")?"\r":t.includes("\r\n")?"\r\n":"\n";return[e.headers=e?.headers?.CSSStyle?["WEBVTT","STYLE"+d+e.headers.CSSboxes].join(d+d):"WEBVTT",e.body=e.body.map((e=>(Array.isArray(e.text)&&(e.text=e.text.join(d)),e=`${e.timeLine} ${e.options}${d}${e.text}`))).join(d+d)].join(d+d)}}(e,t)}
-
-// Not Fininsh
-function EXTM3U(name, opts) {
-	return new (class {
-		constructor(name, opts) {
-			this.name = name
-			Object.assign(this, opts)
-		};
-
-		parse(vtt = new String, options = ["SUBTITLES"]) { // options = ["MEDIA", "AUDIO", "SUBTITLES", "STREAM", "FRAME", "KEY"]
-			const EXTM3U_headers_Regex = /^((?<fileType>\#EXTM3U)[^][^])(?<options>(\#EXT-X-.*)[^][^])?/;
-			const MEDIA_Regex = /^#EXT-X-MEDIA:(.+)$/m;
-			const STREAM_Regex = /^#EXT-X-STREAM-INF:(.+)$/m
-			const FRAME_Regex = /^#EXT-X-I-FRAME-STREAM-INF:(.+)$/m
-			const KEY_Regex = /^#EXT-X-SESSION-KEY:(.+)$/m
-			let json = {
-				headers: vtt.match(webVTT_headers_Regex)?.groups ?? null,
-				body: vtt.split(/[(\r\n)\r\n]{2,}/).map(item => item = item.match(webVTT_body_Regex)?.groups ?? "")
-			};
-			json.body = json.body.filter(Boolean);
-			json.body = json.body.map((item, i) => {
-				item.index = i;
-				if (json.headers?.fileType !== "WEBVTT") {
-					item.timeLine = item.timeLine.replace(",", ".");
-					item.startTime = item.startTime.replace(",", ".");
-					item.endTime = item.endTime.replace(",", ".");
-				}
-				if (options.includes("timeStamp")) {
-					let ISOString = item.startTime.replace(/(.*)/, "1970-01-01T$1Z")
-					item.timeStamp = options.includes("ms") ? Date.parse(ISOString) : Date.parse(ISOString) / 1000;
-				}
-				if (options.includes("multiText")) {
-					item.text = item.text.split(/[(\r\n)\r\n]/);
-				}
-				return item
-			});
-			return json
-		};
-
-		stringify(json = { headers: new Array, body: new Array }, options = ["milliseconds", "\n"]) {
-			const newLine = (options.includes("\n")) ? "\n" : (options.includes("\r")) ? "\r" : (options.includes("\r\n")) ? "\r\n" : "\n";
-			let vtt = [
-				json.headers = (json?.headers?.CSSStyle) ? ["WEBVTT", "STYLE" + newLine + json.headers.CSSboxes].join(newLine + newLine) : "WEBVTT",
-				json.body = json.body.map(item => {
-					if (Array.isArray(item.text)) item.text = item.text.join(newLine);
-					item = `${item.timeLine} ${item.options}${newLine}${item.text}`;
-					return item;
-				}).join(newLine + newLine)
-			].join(newLine + newLine);
-			return vtt
-		};
-	})(name, opts)
-}
+// refer: https://www.w3.org/TR/webvtt1/
+function WebVTT(e,t){return new class{constructor(e,t){this.name=e,Object.assign(this,t)}parse(e=new String,t=["timeStamp"]){const d=t.includes("ms")?/^(?:(?<srtNum>\d+)[(\r\n)\r\n])?(?<timeLine>(?<startTime>(?:\d\d:)?\d\d:\d\d(?:\.|,)\d\d\d) --> (?<endTime>(?:\d\d:)?\d\d:\d\d(?:\.|,)\d\d\d)) ?(?<options>.+)?[^](?<text>.+)/:/^(?:(?<srtNum>\d+)[(\r\n)\r\n])?(?<timeLine>(?<startTime>(?:\d\d:)?\d\d:\d\d)(?:\.|,)\d\d\d --> (?<endTime>(?:\d\d:)?\d\d:\d\d)(?:\.|,)\d\d\d) ?(?<options>.+)?[^](?<text>.+)/;let n={headers:e.match(/^(?<fileType>WEBVTT)?[^](?<Xoptions>.+[^])*/)?.groups??null,CSS:e.match(/^(?<Style>STYLE)[^](?<Boxes>.*::cue.*(\(.*\))?((\n|.)*}$)?)/m)?.groups??null,body:e.split(/[(\r\n)\r\n]{2,}/).map((e=>e.match(d)?.groups??""))};return n.body=n.body.filter(Boolean),n.body=n.body.map(((e,d)=>{if(e.index=d,"WEBVTT"!==n.headers?.fileType&&(e.timeLine=e.timeLine.replace(",","."),e.startTime=e.startTime.replace(",","."),e.endTime=e.endTime.replace(",",".")),t.includes("timeStamp")){let d=e.startTime.replace(/(.*)/,"1970-01-01T$1Z");e.timeStamp=t.includes("ms")?Date.parse(d):Date.parse(d)/1e3}return t.includes("multiText")&&(e.text=e.text.split(/[(\r\n)\r\n]/)),e})),n}stringify(e={headers:{},CSS:{},body:[]},t=["milliseconds","\n"]){const d=t.includes("\n")?"\n":t.includes("\r")?"\r":t.includes("\r\n")?"\r\n":"\n";return[e.headers=e.headers?.Xoptions?[e.headers?.fileType??"WEBVTT",e.headers?.Xoptions??null].join(d):e.headers?.fileType??"WEBVTT",e.CSS=e.CSS?.Style?[e.CSS.Style,e.CSS.Boxes].join(d):null,e.body=e.body.map((e=>(Array.isArray(e.text)&&(e.text=e.text.join(d)),e=`${e.timeLine} ${e.options}${d}${e.text}`))).join(d+d)].join(d+d)}}(e,t)}

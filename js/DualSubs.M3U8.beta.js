63be703840e669a4a4e6d8f1a6bad142ba656ee0
Virgil Clyne <Virgil@nanocat.me>
2022-03-14 22:25:15 +0800

    Update DualSubs.M3U8.beta.js
    Update DualSubs.M3U8.beta.js

--- js/DualSubs.M3U8.beta.js
+++ js/DualSubs.M3U8.beta.js
@@ -22,11 +22,12 @@ let body = $response.body
 	} else {
 		let Parameters = await getParameters(Platform, url);
 		let PlayList = M3U8.parse(body)
-		//$.log(`🚧 ${$.name}`, "M3U8.parse", JSON.stringify(PlayList), "");
+		$.log(`🚧 ${$.name}`, "M3U8.parse", JSON.stringify(PlayList), "");
 		let ENV = {
 			[$.Settings.PreferredLanguage]: await getMEDIA(PlayList, "SUBTITLES", $.Languages[$.Settings.PreferredLanguage]),
 			[$.Settings.SecondaryLanguage]: await getMEDIA(PlayList, "SUBTITLES", $.Languages[$.Settings.SecondaryLanguage])
 		}
+		$.log(`🚧 ${$.name}`, "ENV.stringify", JSON.stringify(ENV), "");
 		/*
 		let ENV = {
 			"1stLanguage": await getSubObj(PlayList, $.Languages[$.Settings.PreferredLanguage]),
@@ -131,9 +132,15 @@ async function getMEDIA(json = {}, type = "", langCode = "") {
 	$.log(`🎉 ${$.name}, 调试信息`, "Get EXT-X-MEDIA Index", `Index: ${index}`, "");
 	let obj = (index != -1) ? json.body[index] : null;
 	$.log(`🎉 ${$.name}, 调试信息`, "Get EXT-X-MEDIA Object", `Object: ${JSON.stringify(obj)}`, "");
-	let uri = obj?.OPTION.URI.replace(/\"/g, "") ?? null;
-	$.log(`🎉 ${$.name}, 调试信息`, "Get EXT-X-MEDIA URI", `URI: ${uri}`, "");
-	let data = { "Index": index, ...obj, "URI": uri}
+	let URI = obj?.OPTION.URI.replace(/\"/g, "") ?? null;
+	// if 相对路径
+	if (!/^https?:\/\//i.test(URI)) {
+		let PATH = url.match(/^(?<PATH>https?:\/\/(?:.+)\/)(?<fileName>[^\/]+\.m3u8)/i)?.groups?.PATH ?? ""
+		//$.log(`🚧 ${$.name}, 调试信息`, "Get Subtitle *.m3u8 URL", `url.match: ${PATH}`, "");
+		URI = (URI == null) ? URI : PATH + URI
+	};
+	$.log(`🎉 ${$.name}, 调试信息`, "Get EXT-X-MEDIA URI", `URI: ${URI}`, "");
+	let data = { "Index": index, ...obj, "URI": URI}
 	$.log(`🎉 ${$.name}, 调试信息`, "Get EXT-X-MEDIA Data", `Data: ${JSON.stringify(data)}`, "");
 	return data
 };
@@ -179,20 +186,20 @@ async function getSubURI(json = {}, path = "") {
 // Set DualSubs Subtitle Array
 async function setDualSubs_Array(obj = {}, lang2ndName = "", type = []) {
 	let newSubs = type.map((item, i) => {
-		$.log(`🎉 ${$.name}, 调试信息`, "Set DualSubs Subtitle Array", `item: ${JSON.stringify(item)}`, "");
+		//$.log(`🎉 ${$.name}, 调试信息`, "Set DualSubs Subtitle Array", `item: ${JSON.stringify(item)}`, "");
 
 		let newSub = JSON.parse(JSON.stringify(obj)) // 复制此语言选项
-		$.log(`🎉 ${$.name}, 调试信息`, "Set DualSubs Subtitle Array", `newSub: ${JSON.stringify(newSub)}`, "");
+		//$.log(`🎉 ${$.name}, 调试信息`, "Set DualSubs Subtitle Array", `newSub: ${JSON.stringify(newSub)}`, "");
 		
 		//newSub.OPTION.NAME = newSub.OPTION.NAME.replace(/^\"([^\/]+)(.*)\"$/, `\"$1/${lang2ndName}(${item})\"`) // 修改名称
 		newSub.OPTION.NAME = `\"${newSub.OPTION.NAME.replace(/\"/g, "")}/${lang2ndName}(${item})\"` // 修改名称
-		$.log(`🎉 ${$.name}, 调试信息`, "Set DualSubs Subtitle Array", `newSub.OPTION.NAME.replace: ${newSub.OPTION.NAME}`, "");
+		//$.log(`🎉 ${$.name}, 调试信息`, "Set DualSubs Subtitle Array", `newSub.OPTION.NAME.replace: ${newSub.OPTION.NAME}`, "");
 		
 		//newSub.OPTION.URI = newSub.OPTION.URI.replace(/^\"([^%%]+)(.*)\"$/, `\"$1%%${item}%%\"`) // 修改链接
 		newSub.OPTION.URI = `\"${newSub.OPTION.URI.replace(/\"/g, "")}%${item}%\"` // 修改链接
-		$.log(`🎉 ${$.name}, 调试信息`, "Set DualSubs Subtitle Array", `newSub.OPTION.URI: ${JSON.stringify(newSub.OPTION.URI)}`, "");
+		//$.log(`🎉 ${$.name}, 调试信息`, "Set DualSubs Subtitle Array", `newSub.OPTION.URI: ${JSON.stringify(newSub.OPTION.URI)}`, "");
 		
-		$.log(`🎉 ${$.name}, 调试信息`, "Set DualSubs Subtitle Array", `newSub: ${JSON.stringify(newSub)}`, "");
+		//$.log(`🎉 ${$.name}, 调试信息`, "Set DualSubs Subtitle Array", `newSub: ${JSON.stringify(newSub)}`, "");
 		return newSub
 	})
 	$.log(`🎉 ${$.name}, 调试信息`, "Set DualSubs Subtitle Array", `newSubs: ${JSON.stringify(newSubs)}`, "");
@@ -248,4 +255,73 @@ async function setCache(cache = {}, parameters = {}, num = new Number) {
 function Env(t,e){class s{constructor(t){this.env=t}send(t,e="GET"){t="string"==typeof t?{url:t}:t;let s=this.get;return"POST"===e&&(s=this.post),new Promise((e,i)=>{s.call(this,t,(t,s,r)=>{t?i(t):e(s)})})}get(t){return this.send.call(this.env,t)}post(t){return this.send.call(this.env,t,"POST")}}return new class{constructor(t,e){this.name=t,this.http=new s(this),this.data=null,this.dataFile="box.dat",this.logs=[],this.isMute=!1,this.isNeedRewrite=!1,this.logSeparator="\n",this.encoding="utf-8",this.startTime=(new Date).getTime(),Object.assign(this,e),this.log("",`🔔${this.name}, 开始!`)}isNode(){return"undefined"!=typeof module&&!!module.exports}isQuanX(){return"undefined"!=typeof $task}isSurge(){return"undefined"!=typeof $httpClient&&"undefined"==typeof $loon}isLoon(){return"undefined"!=typeof $loon}isShadowrocket(){return"undefined"!=typeof $rocket}toObj(t,e=null){try{return JSON.parse(t)}catch{return e}}toStr(t,e=null){try{return JSON.stringify(t)}catch{return e}}getjson(t,e){let s=e;const i=this.getdata(t);if(i)try{s=JSON.parse(this.getdata(t))}catch{}return s}setjson(t,e){try{return this.setdata(JSON.stringify(t),e)}catch{return!1}}getScript(t){return new Promise(e=>{this.get({url:t},(t,s,i)=>e(i))})}runScript(t,e){return new Promise(s=>{let i=this.getdata("@chavy_boxjs_userCfgs.httpapi");i=i?i.replace(/\n/g,"").trim():i;let r=this.getdata("@chavy_boxjs_userCfgs.httpapi_timeout");r=r?1*r:20,r=e&&e.timeout?e.timeout:r;const[o,h]=i.split("@"),a={url:`http://${h}/v1/scripting/evaluate`,body:{script_text:t,mock_type:"cron",timeout:r},headers:{"X-Key":o,Accept:"*/*"}};this.post(a,(t,e,i)=>s(i))}).catch(t=>this.logErr(t))}loaddata(){if(!this.isNode())return{};{this.fs=this.fs?this.fs:require("fs"),this.path=this.path?this.path:require("path");const t=this.path.resolve(this.dataFile),e=this.path.resolve(process.cwd(),this.dataFile),s=this.fs.existsSync(t),i=!s&&this.fs.existsSync(e);if(!s&&!i)return{};{const i=s?t:e;try{return JSON.parse(this.fs.readFileSync(i))}catch(t){return{}}}}}writedata(){if(this.isNode()){this.fs=this.fs?this.fs:require("fs"),this.path=this.path?this.path:require("path");const t=this.path.resolve(this.dataFile),e=this.path.resolve(process.cwd(),this.dataFile),s=this.fs.existsSync(t),i=!s&&this.fs.existsSync(e),r=JSON.stringify(this.data);s?this.fs.writeFileSync(t,r):i?this.fs.writeFileSync(e,r):this.fs.writeFileSync(t,r)}}lodash_get(t,e,s){const i=e.replace(/\[(\d+)\]/g,".$1").split(".");let r=t;for(const t of i)if(r=Object(r)[t],void 0===r)return s;return r}lodash_set(t,e,s){return Object(t)!==t?t:(Array.isArray(e)||(e=e.toString().match(/[^.[\]]+/g)||[]),e.slice(0,-1).reduce((t,s,i)=>Object(t[s])===t[s]?t[s]:t[s]=Math.abs(e[i+1])>>0==+e[i+1]?[]:{},t)[e[e.length-1]]=s,t)}getdata(t){let e=this.getval(t);if(/^@/.test(t)){const[,s,i]=/^@(.*?)\.(.*?)$/.exec(t),r=s?this.getval(s):"";if(r)try{const t=JSON.parse(r);e=t?this.lodash_get(t,i,""):e}catch(t){e=""}}return e}setdata(t,e){let s=!1;if(/^@/.test(e)){const[,i,r]=/^@(.*?)\.(.*?)$/.exec(e),o=this.getval(i),h=i?"null"===o?null:o||"{}":"{}";try{const e=JSON.parse(h);this.lodash_set(e,r,t),s=this.setval(JSON.stringify(e),i)}catch(e){const o={};this.lodash_set(o,r,t),s=this.setval(JSON.stringify(o),i)}}else s=this.setval(t,e);return s}getval(t){return this.isSurge()||this.isLoon()?$persistentStore.read(t):this.isQuanX()?$prefs.valueForKey(t):this.isNode()?(this.data=this.loaddata(),this.data[t]):this.data&&this.data[t]||null}setval(t,e){return this.isSurge()||this.isLoon()?$persistentStore.write(t,e):this.isQuanX()?$prefs.setValueForKey(t,e):this.isNode()?(this.data=this.loaddata(),this.data[e]=t,this.writedata(),!0):this.data&&this.data[e]||null}initGotEnv(t){this.got=this.got?this.got:require("got"),this.cktough=this.cktough?this.cktough:require("tough-cookie"),this.ckjar=this.ckjar?this.ckjar:new this.cktough.CookieJar,t&&(t.headers=t.headers?t.headers:{},void 0===t.headers.Cookie&&void 0===t.cookieJar&&(t.cookieJar=this.ckjar))}get(t,e=(()=>{})){if(t.headers&&(delete t.headers["Content-Type"],delete t.headers["Content-Length"]),this.isSurge()||this.isLoon())this.isSurge()&&this.isNeedRewrite&&(t.headers=t.headers||{},Object.assign(t.headers,{"X-Surge-Skip-Scripting":!1})),$httpClient.get(t,(t,s,i)=>{!t&&s&&(s.body=i,s.statusCode=s.status),e(t,s,i)});else if(this.isQuanX())this.isNeedRewrite&&(t.opts=t.opts||{},Object.assign(t.opts,{hints:!1})),$task.fetch(t).then(t=>{const{statusCode:s,statusCode:i,headers:r,body:o}=t;e(null,{status:s,statusCode:i,headers:r,body:o},o)},t=>e(t));else if(this.isNode()){let s=require("iconv-lite");this.initGotEnv(t),this.got(t).on("redirect",(t,e)=>{try{if(t.headers["set-cookie"]){const s=t.headers["set-cookie"].map(this.cktough.Cookie.parse).toString();s&&this.ckjar.setCookieSync(s,null),e.cookieJar=this.ckjar}}catch(t){this.logErr(t)}}).then(t=>{const{statusCode:i,statusCode:r,headers:o,rawBody:h}=t;e(null,{status:i,statusCode:r,headers:o,rawBody:h},s.decode(h,this.encoding))},t=>{const{message:i,response:r}=t;e(i,r,r&&s.decode(r.rawBody,this.encoding))})}}post(t,e=(()=>{})){const s=t.method?t.method.toLocaleLowerCase():"post";if(t.body&&t.headers&&!t.headers["Content-Type"]&&(t.headers["Content-Type"]="application/x-www-form-urlencoded"),t.headers&&delete t.headers["Content-Length"],this.isSurge()||this.isLoon())this.isSurge()&&this.isNeedRewrite&&(t.headers=t.headers||{},Object.assign(t.headers,{"X-Surge-Skip-Scripting":!1})),$httpClient[s](t,(t,s,i)=>{!t&&s&&(s.body=i,s.statusCode=s.status),e(t,s,i)});else if(this.isQuanX())t.method=s,this.isNeedRewrite&&(t.opts=t.opts||{},Object.assign(t.opts,{hints:!1})),$task.fetch(t).then(t=>{const{statusCode:s,statusCode:i,headers:r,body:o}=t;e(null,{status:s,statusCode:i,headers:r,body:o},o)},t=>e(t));else if(this.isNode()){let i=require("iconv-lite");this.initGotEnv(t);const{url:r,...o}=t;this.got[s](r,o).then(t=>{const{statusCode:s,statusCode:r,headers:o,rawBody:h}=t;e(null,{status:s,statusCode:r,headers:o,rawBody:h},i.decode(h,this.encoding))},t=>{const{message:s,response:r}=t;e(s,r,r&&i.decode(r.rawBody,this.encoding))})}}time(t,e=null){const s=e?new Date(e):new Date;let i={"M+":s.getMonth()+1,"d+":s.getDate(),"H+":s.getHours(),"m+":s.getMinutes(),"s+":s.getSeconds(),"q+":Math.floor((s.getMonth()+3)/3),S:s.getMilliseconds()};/(y+)/.test(t)&&(t=t.replace(RegExp.$1,(s.getFullYear()+"").substr(4-RegExp.$1.length)));for(let e in i)new RegExp("("+e+")").test(t)&&(t=t.replace(RegExp.$1,1==RegExp.$1.length?i[e]:("00"+i[e]).substr((""+i[e]).length)));return t}msg(e=t,s="",i="",r){const o=t=>{if(!t)return t;if("string"==typeof t)return this.isLoon()?t:this.isQuanX()?{"open-url":t}:this.isSurge()?{url:t}:void 0;if("object"==typeof t){if(this.isLoon()){let e=t.openUrl||t.url||t["open-url"],s=t.mediaUrl||t["media-url"];return{openUrl:e,mediaUrl:s}}if(this.isQuanX()){let e=t["open-url"]||t.url||t.openUrl,s=t["media-url"]||t.mediaUrl,i=rawOpts["update-pasteboard"]||rawOpts.updatePasteboard;return{"open-url":e,"media-url":s,"update-pasteboard":i}}if(this.isSurge()){let e=t.url||t.openUrl||t["open-url"];return{url:e}}}};if(this.isMute||(this.isSurge()||this.isLoon()?$notification.post(e,s,i,o(r)):this.isQuanX()&&$notify(e,s,i,o(r))),!this.isMuteLog){let t=["","==============📣系统通知📣=============="];t.push(e),s&&t.push(s),i&&t.push(i),console.log(t.join("\n")),this.logs=this.logs.concat(t)}}log(...t){t.length>0&&(this.logs=[...this.logs,...t]),console.log(t.join(this.logSeparator))}logErr(t,e){const s=!this.isSurge()&&!this.isQuanX()&&!this.isLoon();s?this.log("",`❗️${this.name}, 错误!`,t.stack):this.log("",`❗️${this.name}, 错误!`,t)}wait(t){return new Promise(e=>setTimeout(e,t))}done(t={}){const e=(new Date).getTime(),s=(e-this.startTime)/1e3;this.log("",`🔔${this.name}, 结束! 🕛 ${s} 秒`),this.log(),(this.isSurge()||this.isQuanX()||this.isLoon())&&$done(t)}}(t,e)}
 
 // https://github.com/DualSubs/EXTM3U/blob/main/EXTM3U.min.js
-function EXTM3U(n,e){return new class{constructor(n,e){this.name=n,Object.assign(this,e)}parse(n=new String,e="EXT-X-MEDIA",r=[]){const t=/^(?<EXT>(EXT|AIV)[^:]+):(?<OPTION>.+)([^](?<URI>.+))?/;let o={headers:n.match(/^#(?<fileType>EXTM3U)?[^]*/)?.groups??"",option:n.match(/^#(?<EXT>EXT-X-[^:]+)$/m)?.groups??[],body:n.split(/[(\r\n)\r\n]#/).map((n=>n.match(t)?.groups??""))};return o.body=o.body.map((n=>(n?.EXT==e&&(n.OPTION=Object.fromEntries(n.OPTION.split(",").map((n=>n.split("="))))),n))),o}stringify(n={headers:{},option:[],body:[]},e="EXT-X-MEDIA",r=["\n"]){const t=r.includes("\n")?"\n":r.includes("\r")?"\r":r.includes("\r\n")?"\r\n":"\n";return[n.headers="#"+n.headers.fileType,n.option=n.option.join(t),n.body=n.body.map((n=>{if(n)return n?.EXT==e&&(n.OPTION=Object.entries(n.OPTION).map((n=>n.join("="))).join(",")),"EXT-X-STREAM-INF"==n.EXT?"#"+n.EXT+":"+n.OPTION+t+n.URI:"#"+n.EXT+":"+n.OPTION})).join(t)].join(t)}}(n,e)}
+//function EXTM3U(n,e){return new class{constructor(n,e){this.name=n,Object.assign(this,e)}parse(n=new String,e="EXT-X-MEDIA",r=[]){const t=/^(?<EXT>(EXT|AIV)[^:]+):(?<OPTION>.+)([^](?<URI>.+))?/;let o={headers:n.match(/^#(?<fileType>EXTM3U)?[^]*/)?.groups??"",option:n.match(/^#(?<EXT>EXT-X-[^:]+)$/m)?.groups??[],body:n.split(/[(\r\n)\r\n]#/).map((n=>n.match(t)?.groups??""))};return o.body=o.body.map((n=>(n?.EXT==e&&(n.OPTION=Object.fromEntries(n.OPTION.split(",").map((n=>n.split("="))))),n))),o}stringify(n={headers:{},option:[],body:[]},e="EXT-X-MEDIA",r=["\n"]){const t=r.includes("\n")?"\n":r.includes("\r")?"\r":r.includes("\r\n")?"\r\n":"\n";return[n.headers="#"+n.headers.fileType,n.option=n.option.join(t),n.body=n.body.map((n=>{if(n)return n?.EXT==e&&(n.OPTION=Object.entries(n.OPTION).map((n=>n.join("="))).join(",")),"EXT-X-STREAM-INF"==n.EXT?"#"+n.EXT+":"+n.OPTION+t+n.URI:"#"+n.EXT+":"+n.OPTION})).join(t)].join(t)}}(n,e)}
+
+// refer: https://datatracker.ietf.org/doc/html/draft-pantos-http-live-streaming-08
+function EXTM3U(name, opts) {
+	return new (class {
+		constructor(name, opts) {
+			this.name = name
+			Object.assign(this, opts)
+		};
+
+		parse(m3u8 = "", format = "EXT-X-MEDIA", options = []) {
+			$.log(`🚧 ${$.name}, parse EXTM3U`, "");
+			const EXTM3U_headers_Regex = /^#(?<fileType>EXTM3U)?[^]*/;
+			const EXTM3U_option_Regex = /^#(?<EXT>EXT-X-[^:]+)$/gm;
+			const EXTM3U_body_Regex = /^(?<EXT>(EXT|AIV)[^:]+):(?<OPTION>.+)([^](?<URI>.+))?[^]*$/;
+			let json = {
+				headers: m3u8.match(EXTM3U_headers_Regex)?.groups ?? "",
+				option: m3u8.match(EXTM3U_option_Regex) ?? [],
+				body: m3u8.split(/[^]#/).map(item => item = item.match(EXTM3U_body_Regex)?.groups ?? "")
+			};
+			$.log(`🚧 ${$.name}, parse EXTM3U`, `json.headers: ${JSON.stringify(json.headers)}`, "");
+			$.log(`🚧 ${$.name}, parse EXTM3U`, `json.option: ${JSON.stringify(json.option)}`, "");
+			$.log(`🚧 ${$.name}, parse EXTM3U`, `json.body: ${JSON.stringify(json.body)}`, "");
+			json.body = json.body.map(item => {
+				//$.log(`🚧 ${$.name}, parse EXTM3U`, `before: item.OPTION ${JSON.stringify(item.OPTION)}`, "");
+				$.log(`🚧 ${$.name}, parse EXTM3U`, `before: iitem.OPTION.split(",") ${JSON.stringify(item.OPTION?.split(/,(?=[A-Z])/) ?? "")}`, "");
+				if (item?.EXT == format) item.OPTION = Object.fromEntries(item.OPTION.split(/,(?=[A-Z])/).map(item => item.split("=")));
+				//$.log(`🚧 ${$.name}, parse EXTM3U`, `after: item.OPTION ${JSON.stringify(item.OPTION)}`, "");
+				//$.log(`🚧 ${$.name}, parse EXTM3U`, `after: item ${JSON.stringify(item)}`, "");
+				return item
+			});
+			$.log(`🚧 ${$.name}, parse WebVTT`, `json.body: ${JSON.stringify(json.body)}`, "");
+			return json
+		};
+
+		stringify(json = { headers: {}, option: [], body: [] }, format = "EXT-X-MEDIA", options = ["\n"]) {
+			$.log(`🚧 ${$.name}, stringify EXTM3U`, "");
+			const newLine = (options.includes("\n")) ? "\n" : (options.includes("\r")) ? "\r" : (options.includes("\r\n")) ? "\r\n" : "\n";
+			let m3u8 = [
+				json.headers = "#" + json.headers.fileType,
+				json.option = json.option.join(newLine),
+				body = json.body.map(item => {
+					if (item) {
+						if (item?.EXT == format) {
+							/***************** v0.5.0-beta *****************/
+							item.OPTION = Object.entries(item.OPTION).map(item => item = item.join("=")).join(",");
+							// 按步骤分行写法
+							/*
+							let OPTION = Object.entries(item.OPTION)
+							$.log(`🚧 ${$.name}, stringify EXTM3U`, `Object.entries ${JSON.stringify(OPTION)}`, "");
+
+							OPTION = OPTION.map(item => item = item.join("="))
+							$.log(`🚧 ${$.name}, stringify EXTM3U`, `OPTION.map ${JSON.stringify(OPTION)}`, "");
+
+							OPTION = OPTION.join(",")
+							$.log(`🚧 ${$.name}, stringify EXTM3U`, `OPTION.join ${OPTION}`, "");
+
+							item.OPTION = OPTION
+							*/
+						}
+						return item = (item.EXT == "EXT-X-STREAM-INF") ? "#" + item.EXT + ":" + item.OPTION + newLine + item.URI
+							: "#" + item.EXT + ":" + item.OPTION
+					}
+				}).join(newLine)
+			].join(newLine);
+			$.log(`🚧 ${$.name}, stringify EXTM3U`, `json.body: ${m3u8}`, "");
+			return m3u8
+		};
+	})(name, opts)
+}

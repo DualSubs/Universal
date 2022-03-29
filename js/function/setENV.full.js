9d3fe158543b345fa034881e541a978b8d46b26e
Virgil Clyne <Virgil@nanocat.me>
2022-03-29 09:20:50 +0800

    Update Set Environment Variables Function Files
    setENV.js
    setENV.min.js
    setENV.full.js

--- js/function/setENV.full.js
+++ js/function/setENV.full.js
@@ -2,19 +2,18 @@
 async function setENV(url, database) {
 	$.log(`⚠ ${$.name}, Set Environment Variables`, "");
 	/***************** Platform *****************/
-	let Platform = url.match(/\.(tv|itunes)\.apple\.com/i) ? "Apple_TV"
-		//: url.match(/(play-edge|vod-.*-aoc)\.(tv|itunes)\.apple\.com/i) ? "Apple_TV"
-		//: url.match(/(play|vod-.*-amt)\.(tv|itunes)\.apple\.com/i) ? "iTunes"
-		: url.match(/\.(dssott|starott)\.com/i) ? "Disney_Plus"
-			: url.match(/\.(hls\.row\.aiv-cdn|akamaihd|cloudfront)\.net/i) ? "Prime_Video"
-				: url.match(/\.(api\.hbo|hbomaxcdn)\.com/i) ? "HBO_Max"
-					: url.match(/\.(hulustream|huluim)\.com/i) ? "Hulu"
-						: (url.match(/\.(cbsaavideo|cbsivideo)\.com/i)) ? "Paramount_Plus"
-							: (url.match(/\.peacocktv\.com/i)) ? "Peacock"
-								: url.match(/\.uplynk\.com/i) ? "Discovery_Plus"
-									: url.match(/www\.youtube\.com/i) ? "YouTube"
-										: url.match(/\.nflxvideo\.net/i) ? "Netflix"
-											: undefined
+	let Platform = url.match(/(play|vod-.*-amt)\.(tv|itunes)\.apple\.com/i) ? "Apple_TV"
+		: url.match(/(play-edge|vod-.*-aoc)\.(tv|itunes)\.apple\.com/i) ? "Apple_TV_Plus"
+			: url.match(/\.(dssott|starott)\.com/i) ? "Disney_Plus"
+				: url.match(/\.(hls\.row\.aiv-cdn|akamaihd|cloudfront)\.net/i) ? "Prime_Video"
+					: url.match(/\.(api\.hbo|hbomaxcdn)\.com/i) ? "HBO_Max"
+						: url.match(/\.(hulustream|huluim)\.com/i) ? "Hulu"
+							: (url.match(/\.(cbsaavideo|cbsivideo)\.com/i)) ? "Paramount_Plus"
+								: (url.match(/\.peacocktv\.com/i)) ? "Peacock"
+									: url.match(/\.uplynk\.com/i) ? "Discovery_Plus"
+										: url.match(/www\.youtube\.com/i) ? "YouTube"
+											: url.match(/\.nflxvideo\.net/i) ? "Netflix"
+												: undefined
 	$.log(`🚧 ${$.name}, 调试信息`, "Set Environment Variables", `Platform: ${Platform}`, "");
 	/***************** BoxJs *****************/
 	// 包装为局部变量，用完释放内存
@@ -27,11 +26,9 @@ async function setENV(url, database) {
 	let Settings = BoxJs[Platform]?.Settings || database?.Settings?.[Platform];
 	Settings.Switch = JSON.parse(Settings.Switch) //  BoxJs字符串转Boolean
 	if (typeof Settings.Type == "string") Settings.Type = Settings.Type.split(",") // BoxJs字符串转数组
-	Settings.Type = Settings.Type.filter(e => {
-		if (!Verify.GoogleCloud.Auth) e !== "GoogleCloud";
-		if (!Verify.Azure.Auth) e !== "Azure";
-		if (!Verify.DeepL.Auth) e !== "DeepL";
-	});
+	if (!Verify.GoogleCloud.Auth) Settings.Type = Settings.Type.filter(e => e !== "GoogleCloud"); // 移除不可用类型
+	if (!Verify.Azure.Auth) Settings.Type = Settings.Type.filter(e => e !== "Azure");
+	if (!Verify.DeepL.Auth) Settings.Type = Settings.Type.filter(e => e !== "DeepL");
 	Settings.CacheSize = parseInt(Settings.CacheSize, 10) // BoxJs字符串转数字
 	Settings.Offset = parseInt(Settings.Offset, 10) // BoxJs字符串转数字
 	Settings.Tolerance = parseInt(Settings.Tolerance, 10) // BoxJs字符串转数字
--- js/function/setENV.js
+++ js/function/setENV.js
@@ -2,17 +2,18 @@
 async function setENV(url, database) {
 	//$.log(`⚠ ${$.name}, Set Environment Variables`, "");
 	/***************** Platform *****************/
-	let Platform = url.match(/\.(tv|itunes)\.apple\.com/i) ? "Apple_TV"
-		: url.match(/\.(dssott|starott)\.com/i) ? "Disney_Plus"
-			: url.match(/\.(hls\.row\.aiv-cdn|akamaihd|cloudfront)\.net/i) ? "Prime_Video"
-				: url.match(/\.(api\.hbo|hbomaxcdn)\.com/i) ? "HBO_Max"
-					: url.match(/\.(hulustream|huluim)\.com/i) ? "Hulu"
-						: (url.match(/\.(cbsaavideo|cbsivideo)\.com/i)) ? "Paramount_Plus"
-							: (url.match(/\.peacocktv\.com/i)) ? "Peacock"
-								: url.match(/\.uplynk\.com/i) ? "Discovery_Plus"
-									: url.match(/www\.youtube\.com/i) ? "YouTube"
-										: url.match(/\.nflxvideo\.net/i) ? "Netflix"
-											: undefined
+	let Platform = url.match(/(play|vod-.*-amt)\.(tv|itunes)\.apple\.com/i) ? "Apple_TV"
+		: url.match(/(play-edge|vod-.*-aoc)\.(tv|itunes)\.apple\.com/i) ? "Apple_TV_Plus"
+			: url.match(/\.(dssott|starott)\.com/i) ? "Disney_Plus"
+				: url.match(/\.(hls\.row\.aiv-cdn|akamaihd|cloudfront)\.net/i) ? "Prime_Video"
+					: url.match(/\.(api\.hbo|hbomaxcdn)\.com/i) ? "HBO_Max"
+						: url.match(/\.(hulustream|huluim)\.com/i) ? "Hulu"
+							: (url.match(/\.(cbsaavideo|cbsivideo)\.com/i)) ? "Paramount_Plus"
+								: (url.match(/\.peacocktv\.com/i)) ? "Peacock"
+									: url.match(/\.uplynk\.com/i) ? "Discovery_Plus"
+										: url.match(/www\.youtube\.com/i) ? "YouTube"
+											: url.match(/\.nflxvideo\.net/i) ? "Netflix"
+												: undefined
 	//$.log(`🚧 ${$.name}, 调试信息`, "Set Environment Variables", `Platform: ${Platform}`, "");
 	/***************** BoxJs *****************/
 	// 包装为局部变量，用完释放内存
@@ -25,9 +26,9 @@ async function setENV(url, database) {
 	let Settings = BoxJs[Platform]?.Settings || database?.Settings?.[Platform];
 	Settings.Switch = JSON.parse(Settings.Switch) //  BoxJs字符串转Boolean
 	if (typeof Settings.Type == "string") Settings.Type = Settings.Type.split(",") // BoxJs字符串转数组
-	if (!Verify.GoogleCloud.Auth) Settings.Type.splice(Settings.Type.indexOf("GoogleCloud"), 1);
-	if (!Verify.Azure.Auth) Settings.Type.splice(Settings.Type.indexOf("Azure"), 1);
-	if (!Verify.DeepL.Auth) Settings.Type.splice(Settings.Type.indexOf("DeepL"), 1);
+	if (!Verify.GoogleCloud.Auth) Settings.Type = Settings.Type.filter(e => e !== "GoogleCloud"); // 移除不可用类型
+	if (!Verify.Azure.Auth) Settings.Type = Settings.Type.filter(e => e !== "Azure");
+	if (!Verify.DeepL.Auth) Settings.Type = Settings.Type.filter(e => e !== "DeepL");
 	Settings.CacheSize = parseInt(Settings.CacheSize, 10) // BoxJs字符串转数字
 	Settings.Offset = parseInt(Settings.Offset, 10) // BoxJs字符串转数字
 	Settings.Tolerance = parseInt(Settings.Tolerance, 10) // BoxJs字符串转数字

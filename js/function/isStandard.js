a57346ca61a9bae470b2afc5408a266a25e17666
Virgil Clyne <Virgil@nanocat.me>
2022-03-30 15:39:19 +0800

    Update Determine whether Standard Media Player Function Files

--- js/function/isStandard.js
+++ js/function/isStandard.js
@@ -1,8 +1,9 @@
 // Determine whether Standard Media Player
 async function isStandard(platform, url, headers) {
     $.log(`⚠ ${$.name}, is Standard`, "");
-    if (platform == "Prime_Video" && headers?.Referer.includes("amazon.com")) return false;
-    else if (platform == "HBO_Max") {
-        if (headers?.Referer.includes("hbomax.com")) return false;
-    } else return true;
+    let standard = (platform == "Prime_Video" && headers?.["User-Agent"].includes("Mozilla/5.0")) ? false
+        : (platform == "HBO_Max" && headers?.["User-Agent"].includes("Mozilla/5.0")) ? false
+            : true;
+    $.log(`🎉 ${$.name}, is Standard`, `standard: ${standard}`, "");
+    return standard
 };

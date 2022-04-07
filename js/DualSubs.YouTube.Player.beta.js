4fb6c491830c743b64ec909cb2f905190209bfc9
Virgil Clyne <Virgil@nanocat.me>
2022-04-07 11:31:04 +0800

    Add Determine isStandard
    Update DualSubs.YouTube.Player.beta.js

--- js/DualSubs.YouTube.Player.beta.js
+++ js/DualSubs.YouTube.Player.beta.js
@@ -204,6 +204,21 @@ async function setOptions(Platform = "", Tracklist = {}, Languages1 = [], Langua
 	};
 };
 
+// Determine whether Standard Media Player
+async function isStandard(platform, url, headers) {
+    $.log(`⚠ ${$.name}, is Standard`, "");
+    let standard = true;
+    if (platform == "HBO_Max") {
+		if (headers?.["User-Agent"]?.includes("Mozilla/5.0")) standard = false;
+		else if (headers?.["User-Agent"]?.includes("iPhone")) standard = false;
+		else if (headers?.["User-Agent"]?.includes("iPad")) standard = false;
+        else if (headers?.["X-Hbo-Device-Name"]?.includes("ios")) standard = false;
+        else if (url?.includes("device-code=iphone")) standard = false;
+    }
+    $.log(`🎉 ${$.name}, is Standard`, `standard: ${standard}`, "");
+    return standard
+};
+
 /***************** Env *****************/
 // prettier-ignore
 // https://github.com/chavyleung/scripts/blob/master/Env.min.js

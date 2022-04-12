459b386eef4e9449e6bd154f98662f1b82ee28a0
Virgil Clyne <Virgil@nanocat.me>
2022-04-12 21:46:58 +0800

    Update isStandard.js for Peacock TV
    Update DualSubs.HLS.M3U8.beta.js

--- js/DualSubs.HLS.M3U8.beta.js
+++ js/DualSubs.HLS.M3U8.beta.js
@@ -251,6 +251,11 @@ async function isStandard(platform, url, headers) {
 		else if (headers?.["User-Agent"]?.includes("iPad")) standard = false;
         else if (headers?.["X-Hbo-Device-Name"]?.includes("ios")) standard = false;
         else if (url?.includes("device-code=iphone")) standard = false;
+    } else if (platform == "Peacock_TV") {
+		if (headers?.["User-Agent"]?.includes("Mozilla/5.0")) standard = false;
+		else if (headers?.["User-Agent"]?.includes("iPhone")) standard = false;
+		else if (headers?.["User-Agent"]?.includes("iPad")) standard = false;
+		else if (headers?.["User-Agent"]?.includes("PeacockMobile")) standard = false;
     }
     $.log(`🎉 ${$.name}, is Standard`, `standard: ${standard}`, "");
     return standard

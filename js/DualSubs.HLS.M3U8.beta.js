4bfaa6b08bfba45ecadbf0a9bdd7dfe796ad337c
Virgil Clyne <Virgil@nanocat.me>
2022-04-12 16:12:04 +0800

    Test Get Same Options Index
    Update DualSubs.HLS.M3U8.beta.js

--- js/DualSubs.HLS.M3U8.beta.js
+++ js/DualSubs.HLS.M3U8.beta.js
@@ -220,17 +220,12 @@ async function setOptions(Platform = "", Json = {}, Languages1 = [], Languages2 
 		$.log(`⚠ ${$.name}, Get Same Options Index`, "");
 		// 计算位置
 		let Index = json.body.findIndex(item => {
-			if (platform == "Apple") {
-				if (item?.OPTION?.LANGUAGE == obj?.OPTION?.LANGUAGE
-					&& item?.OPTION?.["GROUP-ID"] == obj?.OPTION?.["GROUP-ID"]
-					&& item?.OPTION?.["STABLE-RENDITION-ID"] == obj?.OPTION?.["STABLE-RENDITION-ID"]) {
-					return true
-				}
-			} else {
-				if (item?.OPTION?.LANGUAGE == obj?.OPTION?.LANGUAGE
-					&& item?.OPTION?.["GROUP-ID"] == obj?.OPTION?.["GROUP-ID"]) {
-					return true
-				}
+			if (item?.OPTION?.LANGUAGE == obj?.OPTION?.LANGUAGE
+				&& item?.OPTION?.["GROUP-ID"] == obj?.OPTION?.["GROUP-ID"]
+				&& item?.OPTION?.CHARACTERISTICS == obj?.OPTION?.CHARACTERISTICS) {
+				if (platform == "Apple") {
+					if (item?.OPTION?.["STABLE-RENDITION-ID"] == obj?.OPTION?.["STABLE-RENDITION-ID"]) return true
+				} else return true
 			}
 		})
 		$.log(`🎉 ${$.name}, Get Same Options Index`, `Index: ${Index}`, "");

4a3936a01bdf5e36bf986a479138c273727dfaa5
Virgil Clyne <Virgil@nanocat.me>
2022-06-16 16:43:11 +0800

    YouTube.TimedText: Test XMLs() v0.1.1
    Update DualSubs.YouTube.TimedText.beta.js

--- js/DualSubs.YouTube.TimedText.beta.js
+++ js/DualSubs.YouTube.TimedText.beta.js
@@ -324,7 +324,7 @@ function WebVTT(e){return new class{constructor(e=["milliseconds","timeStamp","s
 function XMLs(opts) {
 	return new (class {
 		constructor(opts) {
-			this.name = "XML v0.1.0";
+			this.name = "XML v0.1.1";
 			this.opts = opts;
 		};
 
@@ -548,28 +548,23 @@ function XMLs(opts) {
 			return XML;
 			/***************** Fuctions *****************/
 			function toXml(v, name, ind) {
-				var xml = "";
-				if (v instanceof Array) {
-					for (var i = 0, n = v.length; i < n; i++)
+				let xml = "";
+				if (Array.isArray(v)) {
+					for (let i = 0, n = v.length; i < n; i++)
 						xml += ind + toXml(v[i], name, ind + "\t") + "\n";
 				} else if (typeof (v) == "object") {
-					var hasChild = false;
+					let hasChild = false;
 					xml += ind + "<" + name;
-					for (var m in v) {
-						if (m.charAt(0) == "@")
-							xml += " " + m.substr(1) + "=\"" + v[m].toString() + "\"";
-						else
-							hasChild = true;
+					for (let m in v) {
+						if (m.charAt(0) !== "@") hasChild = true;
+						else xml += " " + m.substr(1) + "=\"" + v[m].toString() + "\"";
 					}
 					xml += hasChild ? ">" : "/>";
 					if (hasChild) {
-						for (var m in v) {
-							if (m == "#")
-								xml += v[m];
-							else if (m == "#cdata")
-								xml += "<![CDATA[" + v[m] + "]]>";
-							else if (m.charAt(0) != "@")
-								xml += toXml(v[m], m, ind + "\t");
+						for (let m in v) {
+							if (m == "#") xml += v[m];
+							else if (m == "#cdata") xml += "<![CDATA[" + v[m] + "]]>";
+							else if (m.charAt(0) != "@") xml += toXml(v[m], m, ind + "\t");
 						}
 						xml += (xml.charAt(xml.length - 1) == "\n" ? ind : "") + "</" + name + ">";
 					}

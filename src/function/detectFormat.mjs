db70226d2ab76ff68d2a0ce0faaa39a2c1cf8d55
Virgil Clyne <Virgil@nanocat.me>
2024-01-29 16:50:31 +0800

    test(function): setENV() & detectPlatform() & detectFormat()
    Create setENV.mjs
    Create detectPlatform.mjs
    Create detectFormat.mjs

---
+++ src/function/detectFormat.mjs
@@ -0,0 +1,77 @@
+/**
+ * detect Format
+ * @author VirgilClyne
+ * @param {Object} url - Parsed URL
+ * @param {String} body - response body
+ * @return {String} format - format
+ */
+export default function detectFormat(url, body) {
+	let format = undefined;
+	console.log(`☑️ detectFormat, format: ${url.format ?? url.query?.fmt ?? url.query?.format}`, "");
+	switch (url.format ?? url.query?.fmt ?? url.query?.format) {
+		case "txt":
+			format = "text/plain";
+			break;
+		case "xml":
+		case "srv3":
+		case "ttml":
+		case "ttml2":
+		case "imsc":
+			format = "text/xml";
+			break;
+		case "vtt":
+		case "webvtt":
+			format = "text/vtt";
+			break;
+		case "json":
+		case "json3":
+			format = "application/json";
+			break;
+		case "m3u":
+		case "m3u8":
+			format = "application/x-mpegurl";
+			break;
+		case "plist":
+			format = "application/plist";
+			break;
+		case undefined:
+			const HEADER = body?.substring?.(0, 6).trim?.();
+			//console.log(`🚧 detectFormat, HEADER: ${HEADER}`, "");
+			//console.log(`🚧 detectFormat, HEADER?.substring?.(0, 1): ${HEADER?.substring?.(0, 1)}`, "");
+			switch (HEADER) {
+				case "<?xml":
+					format = "text/xml";
+					break;
+				case "WEBVTT":
+					format = "text/vtt";
+					break;
+				default:
+					switch (HEADER?.substring?.(0, 1)) {
+						case "0":
+						case "1":
+						case "2":
+						case "3":
+						case "4":
+						case "5":
+						case "6":
+						case "7":
+						case "8":
+						case "9":
+							format = "text/vtt";
+							break;
+						case "{":
+							format = "application/json";
+							break;
+						case undefined:
+						default:
+							break;
+					};
+					break;
+				case undefined:
+					break;
+			};
+			break;
+	};
+	console.log(`✅ detectFormat, format: ${format}`, "");
+	return format;
+};
---
+++ src/function/setENV.mjs
@@ -0,0 +1,67 @@
+/*
+README: https://github.com/DualSubs
+*/
+
+import ENVs from "../ENV/ENV.mjs";
+const $ = new ENVs("🍿️ DualSubs: Set Environment Variables");
+
+/**
+ * Set Environment Variables
+ * @author VirgilClyne
+ * @param {String} name - Persistent Store Key
+ * @param {Array} platforms - Platform Names
+ * @param {Object} database - Default DataBase
+ * @return {Object} { Settings, Caches, Configs }
+ */
+export default function setENV(name, platforms, database) {
+	$.log(`☑️ ${$.name}`, "");
+	let { Settings, Caches, Configs } = $.getENV(name, platforms, database);
+	/***************** Settings *****************/
+	if (!Array.isArray(Settings?.Types)) Settings.Types = (Settings.Types) ? [Settings.Types] : []; // 只有一个选项时，无逗号分隔
+	if ($.isLoon() && platforms.includes("YouTube")) {
+		Settings.AutoCC = $persistentStore.read("自动显示翻译字幕") ?? Settings.AutoCC;
+		switch (Settings.AutoCC) {
+			case "是":
+				Settings.AutoCC = true;
+				break;
+			case "否":
+				Settings.AutoCC = false;
+				break;
+			default:
+				break;
+		};
+		Settings.ShowOnly = $persistentStore.read("仅输出译文") ?? Settings.ShowOnly;
+		switch (Settings.ShowOnly) {
+			case "是":
+				Settings.ShowOnly = true;
+				break;
+			case "否":
+				Settings.ShowOnly = false;
+				break;
+			default:
+				break;
+		};
+		Settings.Position = $persistentStore.read("字幕译文位置") ?? Settings.Position;
+		switch (Settings.Position) {
+			case "译文位于外文之上":
+				Settings.Position = "Forward";
+				break;
+			case "译文位于外文之下":
+				Settings.Position = "Reverse";
+				break;
+			default:
+				break;
+		};
+	};
+	$.log(`✅ ${$.name}, Set Environment Variables`, `Settings: ${typeof Settings}`, `Settings内容: ${JSON.stringify(Settings)}`, "");
+	/***************** Caches *****************/
+	//$.log(`✅ ${$.name}, Set Environment Variables`, `Caches: ${typeof Caches}`, `Caches内容: ${JSON.stringify(Caches)}`, "");
+	if (typeof Caches?.Playlists !== "object" || Array.isArray(Caches?.Playlists)) Caches.Playlists = {}; // 创建Playlists缓存
+	Caches.Playlists.Master = new Map(JSON.parse(Caches?.Playlists?.Master || "[]")); // Strings转Array转Map
+	Caches.Playlists.Subtitle = new Map(JSON.parse(Caches?.Playlists?.Subtitle || "[]")); // Strings转Array转Map
+	if (typeof Caches?.Subtitles !== "object") Caches.Subtitles = new Map(JSON.parse(Caches?.Subtitles || "[]")); // Strings转Array转Map
+	if (typeof Caches?.Metadatas !== "object" || Array.isArray(Caches?.Metadatas)) Caches.Metadatas = {}; // 创建Playlists缓存
+	if (typeof Caches?.Metadatas?.Tracks !== "object") Caches.Metadatas.Tracks = new Map(JSON.parse(Caches?.Metadatas?.Tracks || "[]")); // Strings转Array转Map
+	/***************** Configs *****************/
+	return { Settings, Caches, Configs };
+};

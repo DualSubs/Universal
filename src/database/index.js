36fbe4114392e190415c7d65abf6dffddca7173a
Virgil Clyne <Virgil@nanocat.me>
2024-01-30 09:53:22 +0800

    docs(database): index.js
    Create Database.json
    Create index.js

---
+++ src/database/index.js
@@ -0,0 +1,21 @@
+import * as Default from "./Default.json";
+import * as Universal from "./Universal.json";
+import * as YouTube from "./YouTube.json";
+import * as Netflix from "./Netflix.json";
+import * as Spotify from "./Spotify.json";
+import * as Composite from "./Composite.json";
+import * as Translate from "./Translate.json";
+import * as External from "./External.json";
+import * as API from "./API.json";
+
+export default Database = {
+	"Default": Default,
+	"Universal": Universal,
+	"YouTube": YouTube,
+	"Netflix": Netflix,
+	"Spotify": Spotify,
+	"Composite": Composite,
+	"Translate": Translate,
+	"External": External,
+	"External": API,
+};

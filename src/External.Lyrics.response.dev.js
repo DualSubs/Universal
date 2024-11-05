16a5c397f0da031c02344f5fd9b0b33da67febaf
Virgil Clyne <Virgil@nanocat.me>
2024-11-05 10:29:09 +0800

    refactor(External.Lyrics.response): Spotify
    Update External.Lyrics.response.dev.js
    Update External.Lyrics.response.js

--- src/External.Lyrics.response.dev.js
+++ src/External.Lyrics.response.dev.js
@@ -1,11 +1,11 @@
-import { $platform, URL, Lodash as _, Storage, fetch, notification, log, logError, wait, done, getScript, runScript } from "@nsnanocat/util";
+import { $platform, Lodash as _, Storage, fetch, notification, log, logError, wait, done } from "@nsnanocat/util";
 import LRC from "./LRC/LRC.mjs";
 import database from "./database/index.mjs";
 import setENV from "./function/setENV.mjs";
 import detectPlatform from "./function/detectPlatform.mjs";
 import setCache from "./function/setCache.mjs";
-import { TextEncoder , TextDecoder } from "text-encoding";
-import { WireType, UnknownFieldHandler, reflectionMergePartial, MESSAGE_TYPE, MessageType, BinaryReader, isJsonObject, typeofJsonValue, jsonWriteOptions } from "@protobuf-ts/runtime";
+import { BrowseResponse } from "./protobuf/google/protos/youtube/api/innertube/BrowseResponse.js";
+import { ColorLyricsResponse } from "./protobuf/spotify/lyrics/Lyrics.js";
 /***************** Processing *****************/
 // 解构URL
 const url = new URL($request.url);
@@ -102,101 +102,9 @@ log(`⚠ FORMAT: ${FORMAT}`, "");
 						case "application/vnd.google.protobuf":
 							switch (PLATFORM) {
 								case "YouTube": {
-									/******************  initialization start  *******************/
-									/******************  initialization finish  *******************/
 									break;
 								}
 								case "Spotify": {
-									/******************  initialization start  *******************/
-									var SyncType;
-									(function (SyncType) {
-										SyncType[SyncType["UNSYNCED"] = 0] = "UNSYNCED";
-										SyncType[SyncType["LINE_SYNCED"] = 1] = "LINE_SYNCED";
-										SyncType[SyncType["SYLLABLE_SYNCED"] = 2] = "SYLLABLE_SYNCED";
-									})(SyncType || (SyncType = {}));
-									class ColorLyricsResponse$Type extends MessageType {
-										constructor() {
-											super("ColorLyricsResponse", [
-												{ no: 1, name: "lyrics", kind: "message", T: () => LyricsResponse },
-												{ no: 2, name: "colors", kind: "message", T: () => ColorData },
-												{ no: 3, name: "hasVocalRemoval", kind: "scalar", opt: true, T: 8 /*ScalarType.BOOL*/ },
-												{ no: 4, name: "vocalRemovalColors", kind: "message", T: () => ColorData }
-											]);
-										}
-									}
-									const ColorLyricsResponse = new ColorLyricsResponse$Type();
-									class LyricsResponse$Type extends MessageType {
-										constructor() {
-											super("LyricsResponse", [
-												{ no: 1, name: "syncType", kind: "enum", T: () => ["SyncType", SyncType] },
-												{ no: 2, name: "lines", kind: "message", repeat: 1 /*RepeatType.PACKED*/, T: () => LyricsLine },
-												{ no: 3, name: "provider", kind: "scalar", T: 9 /*ScalarType.STRING*/ },
-												{ no: 4, name: "providerLyricsId", kind: "scalar", T: 9 /*ScalarType.STRING*/ },
-												{ no: 5, name: "providerDisplayName", kind: "scalar", T: 9 /*ScalarType.STRING*/ },
-												{ no: 6, name: "syncLyricsAndroidIntent", kind: "message", T: () => AndroidIntent },
-												{ no: 7, name: "syncLyricsUri", kind: "scalar", T: 9 /*ScalarType.STRING*/ },
-												{ no: 8, name: "isDenseTypeface", kind: "scalar", T: 8 /*ScalarType.BOOL*/ },
-												{ no: 9, name: "alternatives", kind: "message", repeat: 1 /*RepeatType.PACKED*/, T: () => Alternative },
-												{ no: 10, name: "language", kind: "scalar", T: 9 /*ScalarType.STRING*/ },
-												{ no: 11, name: "isRtlLanguage", kind: "scalar", opt: true, T: 8 /*ScalarType.BOOL*/ },
-												{ no: 12, name: "fullscreenAction", kind: "scalar", T: 5 /*ScalarType.INT32*/ },
-												{ no: 13, name: "showUpsell", kind: "scalar", opt: true, T: 8 /*ScalarType.BOOL*/ }
-											]);
-										}
-									}
-									const LyricsResponse = new LyricsResponse$Type();
-									class LyricsLine$Type extends MessageType {
-										constructor() {
-											super("LyricsLine", [
-												{ no: 1, name: "startTimeMs", kind: "scalar", T: 3 /*ScalarType.INT64*/ },
-												{ no: 2, name: "words", kind: "scalar", opt: true, T: 9 /*ScalarType.STRING*/ },
-												{ no: 3, name: "syllables", kind: "message", repeat: 1 /*RepeatType.PACKED*/, T: () => Syllable },
-												//{ no: 4, name: "endTimeMs", kind: "scalar", opt: true, T: 3 /*ScalarType.INT64*/ }
-											]);
-										}
-									}
-									const LyricsLine = new LyricsLine$Type();
-									class Syllable$Type extends MessageType {
-										constructor() {
-											super("Syllable", [
-												{ no: 1, name: "startTimeMs", kind: "scalar", T: 3 /*ScalarType.INT64*/ },
-												{ no: 2, name: "numChars", kind: "scalar", T: 3 /*ScalarType.INT64*/ }
-											]);
-										}
-									}
-									const Syllable = new Syllable$Type();
-									class ColorData$Type extends MessageType {
-										constructor() {
-											super("ColorData", [
-												{ no: 1, name: "background", kind: "scalar", T: 5 /*ScalarType.INT32*/ },
-												{ no: 2, name: "text", kind: "scalar", T: 5 /*ScalarType.INT32*/ },
-												{ no: 3, name: "highlightText", kind: "scalar", T: 5 /*ScalarType.INT32*/ }
-											]);
-										}
-									}
-									const ColorData = new ColorData$Type();
-									class AndroidIntent$Type extends MessageType {
-										constructor() {
-											super("AndroidIntent", [
-												{ no: 1, name: "provider", kind: "scalar", T: 9 /*ScalarType.STRING*/ },
-												{ no: 2, name: "providerAndroidAppId", kind: "scalar", T: 9 /*ScalarType.STRING*/ },
-												{ no: 3, name: "action", kind: "scalar", T: 9 /*ScalarType.STRING*/ },
-												{ no: 4, name: "data", kind: "scalar", T: 9 /*ScalarType.STRING*/ },
-												{ no: 5, name: "contentType", kind: "scalar", T: 9 /*ScalarType.STRING*/ }
-											]);
-										}
-									}
-									const AndroidIntent = new AndroidIntent$Type();
-									class Alternative$Type extends MessageType {
-										constructor() {
-											super("Alternative", [
-												{ no: 1, name: "language", kind: "scalar", T: 9 /*ScalarType.STRING*/ },
-												{ no: 2, name: "lines", kind: "scalar", repeat: 2 /*RepeatType.UNPACKED*/, T: 9 /*ScalarType.STRING*/ }
-											]);
-										}
-									}
-									const Alternative = new Alternative$Type();
-									/******************  initialization finish  *******************/
 									body = ColorLyricsResponse.fromBinary(rawBody);
 									log(`🚧 body: ${JSON.stringify(body)}`, "");
 									/*
@@ -369,6 +277,7 @@ async function injectionLyric(vendor = "NeteaseMusic", trackInfo = {}, body = $r
 					case "Win32_x86_64": // Windows App 暂不支持翻译功能
 					case "WebPlayer": // Web App
 					case undefined:
+					// biome-ignore lint/suspicious/noFallthroughSwitchClause: <explanation>
 					default:
 						/*
 						if (duolyric) body.lyrics.lines = body.lyrics.lines.map((line, i) => {

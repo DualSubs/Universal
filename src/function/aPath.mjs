22415fadd05adbda0b821b233659dc3579f84518
脆薯饼 <Virgil@nanocat.me>
2024-12-19 16:05:30 +0800

    fix(aPath): startsWith("/")
    Update aPath.mjs

--- src/function/aPath.mjs
+++ src/function/aPath.mjs
@@ -1,4 +1,17 @@
 // Get Absolute Path
-export default function aPath(aURL = "", URL = "") {
-	return /^https?:\/\//i.test(URL) ? URL : aURL.match(/^(https?:\/\/(?:[^?]+)\/)/i)?.[0] + URL;
+export default function aPath(URL = "", URI = "") {
+	let url = "";
+	switch (true) {
+		case URI.startsWith("https://") || URI.startsWith("http://"):
+			url = URI;
+			break;
+		case URI.startsWith("/"):
+			url = URL.match(/^(https?:\/\/(?:[^/]+))/i)?.[0] + URI;
+			break;
+		default:
+			url = URL.match(/^(https?:\/\/(?:[^?]+)\/)/i)?.[0] + URI;
+			break;
+	}
+	return url;
+	//return /^https?:\/\//i.test(URI) ? URI : URL.match(/^(https?:\/\/(?:[^?]+)\/)/i)?.[0] + URI;
 }
